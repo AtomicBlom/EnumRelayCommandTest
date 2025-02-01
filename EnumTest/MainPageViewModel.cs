ea74b1a1ab0b7eee668966a6f2af652f52c93638
SteveBlom <SteveBlom@TRINITY>
2025-02-02 02:23:33 +1030

    Add project files.

---
+++ EnumTest/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace EnumTest
+{
+	public partial class MainPageViewModel : ObservableObject
+	{
+		[RelayCommand]
+		private void OnSetSortOrder(SortOrder order)
+		{
+			Debug.WriteLine($"Sort order set to {order}");
+			SortOrder = order;
+		}
+
+		private SortOrder _sortOrder;
+		public SortOrder SortOrder
+		{
+			get => _sortOrder;
+			set => SetProperty(ref _sortOrder, value);
+		}
+
+	}
+
+	public enum SortOrder
+	{
+		Ascending,
+		Descending
+	}
+}
